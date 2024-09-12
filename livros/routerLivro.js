b78fa2dff039d32bc67a025bc3747f957cf0649c
brunoscheifer <brunoscheifer11@gmail.com>
2024-09-12 14:49:03 -0300

    melhorei livros

--- livros/routerLivro.js
+++ livros/routerLivro.js
@@ -2,22 +2,27 @@ const express = require('express')
 const router = express.Router()
 
 const criarLivro = require('./criarLivro')
-const listarLivros = require('./listarLivros')
-const editarLivro = require('./criarLivro')
-const deletarLivro = require('./deleteLivro');
-const buscarLivrosPorAutor = require('./buscarLivrosPorAutor')
-const buscarLivrosPorAno = require('./buscarLivrosPorAno')
-const buscarLivrosPorGenero = require('./buscarLivrosPorGenero')
-const buscarLivrosPorTitulo = require('./buscarLivrosPorTitulo')
-
 router.post('/', criarLivro)
+
+const listarLivros = require('./listarLivros')
 router.get('/', listarLivros)
+
+const editarLivro = require('./criarLivro')
 router.put('/:id:', editarLivro)
+
+const deletarLivro = require('./deleteLivro');
 router.delete('/:id', deletarLivro)
 
+const buscarLivrosPorGenero = require('./buscarLivrosPorGenero')
 router.get('/genero', buscarLivrosPorGenero)
+
+const buscarLivrosPorAno = require('./buscarLivrosPorAno')
 router.get('/ano', buscarLivrosPorAno)
+
+const buscarLivrosPorTitulo = require('./buscarLivrosPorTitulo')
 router.get('/titulo', buscarLivrosPorTitulo)
+
+const buscarLivrosPorAutor = require('./buscarLivrosPorAutor')
 router.get('/autor', buscarLivrosPorAutor)
 
 module.exports = router
